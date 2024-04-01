b520757a91183d346e5b2c3d65dbd59b0b83e6ae
jisulim <jisulim0@gmail.com>
2024-04-01 16:04:49 +0900

    feat : wireframe LoginActivity 화면 개발

--- app/src/main/java/com/sideprojhub/hub/android/ui/LoginActivity.kt
+++ app/src/main/java/com/sideprojhub/hub/android/ui/LoginActivity.kt
@@ -2,19 +2,20 @@ package com.sideprojhub.hub.android.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.sideprojhub.hub.android.BaseActivity
 import com.sideprojhub.hub.android.R
-import com.sideprojhub.hub.android.databinding.ActivityMainBinding
+import com.sideprojhub.hub.android.databinding.ActivityLoginBinding
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
-    private val binding by binding<ActivityMainBinding>(R.layout.activity_main)
+    private val binding by binding<ActivityLoginBinding>(R.layout.activity_login)
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding.lifecycleOwner = this@MainActivity
+        binding.lifecycleOwner = this@LoginActivity
     }
+
+
 }
